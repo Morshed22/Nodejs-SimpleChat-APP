cc71c86b724b4e34c6ee0654df58cef881fec9d2
Morshed Alam <morshed.alam@gagagugu.com>
2018-07-19 01:33:20 +0600

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -3,19 +3,35 @@ var socket = io();
         console.log('Connected to server');
     });
     
+    function scrollToBottom () {
+        // Selectors
+        var messages = jQuery('#messages');
+        var newMessage = messages.children('li:last-child')
+        // Heights
+        var clientHeight = messages.prop('clientHeight');
+        var scrollTop = messages.prop('scrollTop');
+        var scrollHeight = messages.prop('scrollHeight');
+        var newMessageHeight = newMessage.innerHeight();
+        var lastMessageHeight = newMessage.prev().innerHeight();
+      
+        if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+          messages.scrollTop(scrollHeight);
+        }
+      }
+
     socket.on('disconnect', function(){
         console.log('Disconnect from server');
     });
     socket.on('newMessage', function(message){
         var formattedTime = moment(message.createdAt).format('h:mm a');
         var template = jQuery('#message-template').html();
-        var html = Mustache.render(template, {
+        var html = Mustache.render(template, { 
             text:message.text,
             from:message.from,
             createdAt:formattedTime
         });
         jQuery('#messages').append(html);
-
+        scrollToBottom();
     });
 
     socket.emit('createMessage',{
@@ -34,12 +50,7 @@ var socket = io();
         });
         jQuery('#messages').append(html);
 
-        // var li = jQuery('<li></li>');
-        // var a = jQuery('<a target="_blank">current location</a>');
-        // li.text(`${message.from} ${formattedTime}:`)
-        // a.attr('href',message.url);
-        // li.append(a);
-        // jQuery('#messages').append(li);
+        scrollToBottom();
 
     });
 
